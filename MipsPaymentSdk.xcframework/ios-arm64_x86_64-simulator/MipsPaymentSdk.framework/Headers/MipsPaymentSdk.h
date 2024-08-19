3489479fa2aa5b98c9aba17d5d2290c4961e7a5f
shyank akshar <akshar@shyankdev.us>
2024-08-20 02:41:33 +0530

    fix framework

---
+++ MipsPaymentSdk.xcframework/ios-arm64_x86_64-simulator/MipsPaymentSdk.framework/Headers/MipsPaymentSdk.h
@@ -0,0 +1,17 @@
+//
+//  MipsPaymentSdk.h
+//  MipsPaymentSdk
+//
+//  Created by shyank on 20/08/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MipsPaymentSdk.
+FOUNDATION_EXPORT double MipsPaymentSdkVersionNumber;
+
+//! Project version string for MipsPaymentSdk.
+FOUNDATION_EXPORT const unsigned char MipsPaymentSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MipsPaymentSdk/PublicHeader.h>
+
